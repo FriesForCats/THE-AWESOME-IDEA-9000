f193b9d3f9f14be08640d6b1e6701962cbc68954
Andrew <ajf036@latech.edu>
2026-04-01 15:32:32 -0500

    Close with Esc / x button now

--- camera_first_try.py
+++ camera_first_try.py
@@ -2,7 +2,7 @@ import cv2
 from time import sleep
 from matplotlib import pyplot as plt
 
-cap =cv2.VideoCapture(1)
+cap =cv2.VideoCapture(1) # Set to 0 for built in webcam if 1 does not work
 
 
 while True:
@@ -20,11 +20,11 @@ while True:
     #used to kill camera stream
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    if cv2.waitKey(1) & 0xFF == ord('\x1b'): # ESC key wworks to delete video feed
+        break
+    if cv2.getWindowProperty("feed", cv2.WND_PROP_VISIBLE) < 1:
+        break
 
 #releases camera and windows captured
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
