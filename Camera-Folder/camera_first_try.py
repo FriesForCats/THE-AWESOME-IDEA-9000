dfb58c93ae8805b00c92b8facb0dc7029dd53473
Christopher <christopherckirk@gmail.com>
2026-04-08 13:33:39 -0500

    Camera detects faces

--- Camera-Folder/camera_first_try.py
+++ Camera-Folder/camera_first_try.py
@@ -1,8 +1,16 @@
 import cv2
 from time import sleep
 from matplotlib import pyplot as plt
+import numpy as np
+
+
+prototxt_path = "deploy.prototxt"
+model_path = "res10_300x300_ssd_iter_140000.caffemodel"
+
+net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
 
 cap =cv2.VideoCapture(1) # Set to 0 for built in webcam if 1 does not work
+
 
 
 while True:
@@ -14,10 +22,43 @@ while True:
         print("Error: Failed to capture image.")
         break
     
-    #shows frame in a window
+    #gets the height and width of the camera feed for later
+    h, w = frame.shape[:2]
+    
+    # Pre-process: create a 300x300 blob 
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,(300, 300), (104.0, 177.0, 123.0))
+    "This line ^ shrinks the image into a 300 x 300 square "
+    "and then scales it to stay 300 x 300p "
+    "and finally takes all the color out"
+   
+    # Run detection
+    net.setInput(blob)
+    detections = net.forward()
+
+    # Loop over detections
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        # Filter out weak detections (< 50% confidence)
+        if confidence > 0.5:
+            # Scale coordinates back to original frame size 
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+
+            # Converts the confidence of the face into a percent
+            text = f"{confidence * 100:.2f}%"
+            
+            # Box drawing
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+            # Percent Drawing
+            cv2.putText(frame, text, (startX, startY - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+            
+    #Shows the image in a life feed
     cv2.imshow("feed",frame)
 
-    #used to kill camera stream
+    #Used to kill camera stream
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     if cv2.waitKey(1) & 0xFF == ord('\x1b'): # ESC key wworks to delete video feed
@@ -25,6 +66,6 @@ while True:
     if cv2.getWindowProperty("feed", cv2.WND_PROP_VISIBLE) < 1:
         break
 
-#releases camera and windows captured
+#Releases camera and windows captured
 cap.release()
 cv2.destroyAllWindows()
